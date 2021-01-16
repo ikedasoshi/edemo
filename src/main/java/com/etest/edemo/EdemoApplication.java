4a9ff1cdc371fccc33533ee8a038ee72a223d15c
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-16 16:11:00 +0900

    0116

---
+++ src/main/java/com/etest/edemo/EdemoApplication.java
@@ -0,0 +1,13 @@
+package com.etest.edemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdemoApplication.class, args);
+	}
+
+}
